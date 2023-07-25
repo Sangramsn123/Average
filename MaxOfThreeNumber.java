b8b8627f3f258b873f05fe8a94012af53c8a91d2
Sangramsn123 <140451302+Sangramsn123@users.noreply.github.com>
2023-07-25 09:17:43 +0530

    Update MaxOfThreeNumber.java

--- MaxOfThreeNumber.java
+++ MaxOfThreeNumber.java
@@ -21,6 +21,10 @@ public class MaxOfThreeNumber {
 			System.out.println("Largest Number = " + n3);
 		}
 
+		int avg = (n1+n2+n3)/3;
+		System.out.println("Average Of Three Number  = " + avg);
+		
+
 	}
 
 }
