cccdb0dc1982b68cce7f24fa3ab00480e8de4fec
Sangramsn123 <nipane.sangram123@gmail.com>
2023-07-25 08:33:38 +0530

    MaxOfThreeNumber

---
+++ MaxOfThreeNumber.java
@@ -0,0 +1,26 @@
+package abcd;
+
+import java.util.Scanner;
+
+public class MaxOfThreeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number 1 : ");
+		int n1 = sc.nextInt();
+		System.out.println("Enter Number 2 : ");
+		int n2 = sc.nextInt();
+		System.out.println("Enter Number 3 : ");
+		int n3 = sc.nextInt();
+
+		if (n1 >= n2 && n1 >= n3) {
+			System.out.println("Largest Number = " + n1);
+		} else if (n2 >= n1 && n2 >= n3) {
+			System.out.println("Largest Number = " + n2);
+		} else {
+			System.out.println("Largest Number = " + n3);
+		}
+
+	}
+
+}
